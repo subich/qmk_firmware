8301f2f6c1a50773929887979650662cc35791b5
Stefan Subich <su-stefan@users.noreply.github.com>
2021-06-27 20:17:45 -0400

    userspace cleanup

--- users/su-stefan/su-stefan.h
+++ users/su-stefan/su-stefan.h
@@ -1,20 +1,5 @@
 #include "wrappers.h"
 
-enum layers {
-  _QWERTY,
-  _COLEMAK,
-  _GAME,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
-  GAME
-};
-
 #define _______ KC_TRNS
 #define ___X___ KC_NO
 
