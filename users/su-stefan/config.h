912feb2a01c44ff051f77baf3b9ea0b8615c95ff
Stefan <--global>
2021-06-30 18:47:51 -0400

    cleanup

--- users/su-stefan/config.h
+++ users/su-stefan/config.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#ifdef OLED_DRIVER_ENABLE
-#    ifdef SPLIT_KEYBOARD
-#        define OLED_UPDATE_INTERVAL 60
-#    else
-#        define OLED_UPDATE_INTERVAL 15
-#    endif
-#    define OLED_DISABLE_TIMEOUT
-#    ifdef OLED_FONT_H
-#        undef OLED_FONT_H
-#    endif
-#    define OLED_FONT_H "drashna_font.h"
-#    define OLED_FONT_END 255
+//#ifdef OLED_DRIVER_ENABLE
+//#    ifdef SPLIT_KEYBOARD
+//#        define OLED_UPDATE_INTERVAL 60
+//#    else
+//#        define OLED_UPDATE_INTERVAL 15
+//#    endif
+//#    define OLED_DISABLE_TIMEOUT
+//#    ifdef OLED_FONT_H
+//#        undef OLED_FONT_H
+//#    endif
+//#    define OLED_FONT_H "drashna_font.h"
+//#    define OLED_FONT_END 255
 // #    define OLED_FONT_5X5
 // #    define OLED_FONT_AZTECH
 // #    define OLED_FONT_BMPLAIN
@@ -20,8 +20,9 @@
 // #    define OLED_LOGO_HUE_MANITEE
 // #    define OLED_LOGO_CORNE
 // #    define OLED_LOGO_GOTHAM
-#    define OLED_LOGO_SCIFI
-#endif
+//
+//#    define OLED_LOGO_SCIFI
+//#endif
 
 /* Disable unused and unneeded features to reduce on firmware size */
 #ifdef LOCKING_SUPPORT_ENABLE
--- users/su-stefan/wrappers.h
+++ users/su-stefan/wrappers.h
@@ -52,8 +52,8 @@
 #define _________________ADJUST_L2_________________       MU_TOG , CK_TOGG, AU_ON,   AU_OFF,  CG_NORM
 #define _________________ADJUST_L3_________________       RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, KC_RGB_T
 
-#define _________________ADJUST_R1_________________       KC_SEC1, KC_SEC2, KC_SEC3, KC_SEC4, KC_SEC5
-#define _________________ADJUST_R2_________________       CG_SWAP, QWERTY,  COLEMAK, DVORAK,  WORKMAN
+#define _________________ADJUST_R1_________________       ___X___, ___X___, ___X___, ___X___, ___X___
+#define _________________ADJUST_R2_________________       CG_SWAP, QWERTY,  COLEMAK, ___X___, ___X___
 #define _________________ADJUST_R3_________________       MG_NKRO, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
 
 // clang-format on
