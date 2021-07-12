7a6fa74ae965be7247e0eb972a63a760d14cfb6e
Stefan <--global>
2021-07-11 23:16:07 -0400

    update wrappers

--- users/su-stefan/wrappers.h
+++ users/su-stefan/wrappers.h
@@ -33,8 +33,8 @@
 #define _________________LOWER_L3__________________       ___________________BLANK___________________
 
 #define _________________LOWER_R1__________________       ________________NUMBER_RIGHT_______________
-#define _________________LOWER_R2__________________       _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR
-#define _________________LOWER_R3__________________       _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+#define _________________LOWER_R2__________________       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___X___
+#define _________________LOWER_R3__________________       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  ___X___
 
 
 #define _________________RAISE_L1__________________       KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC
@@ -42,16 +42,16 @@
 #define _________________RAISE_L3__________________       _________________FUNC_RIGHT________________
 
 #define _________________RAISE_R1__________________       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN
-#define _________________RAISE_R2__________________       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC
-#define _________________RAISE_R3__________________       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+#define _________________RAISE_R2__________________       KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE
+#define _________________RAISE_R3__________________       KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS
 
 
 #define _________________ADJUST_L1_________________       RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG
 #define _________________ADJUST_L2_________________       MU_TOG , CK_TOGG, AU_ON,   AU_OFF,  CG_NORM
 #define _________________ADJUST_L3_________________       RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, ___X___
 
-#define _________________ADJUST_R1_________________       ___X___, ___X___, ___X___, ___X___, ___X___
-#define _________________ADJUST_R2_________________       CG_SWAP, QWERTY,  COLEMAK, ___X___, ___X___
-#define _________________ADJUST_R3_________________       ___X___, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
+#define _________________ADJUST_R1_________________       ___X___, KC_MUTE, ___X___, ___X___, ___X___
+#define _________________ADJUST_R2_________________       KC_PAUS, KC_VOLU, COLEMAK, ___X___, ___X___
+#define _________________ADJUST_R3_________________       ___X___, KC_VOLD, KC_VOLD, KC_VOLU, KC_MNXT
 
 // clang-format on
