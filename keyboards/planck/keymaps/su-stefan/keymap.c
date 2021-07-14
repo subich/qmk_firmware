411e8708727e16961b9887f43c74445c364124b9
Stefan <--global>
2021-07-13 22:28:12 -0400

    update planck layout for userspace/wrappers

--- keyboards/planck/keymaps/su-stefan/keymap.c
+++ keyboards/planck/keymaps/su-stefan/keymap.c
@@ -22,38 +22,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAME] = LAYOUT_planck_grid_wrapper(
-    KC_1,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_1,    _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSPC,
+    KC_LSFT, _________________QWERTY_L2_________________, _________________QWERTY_R1_________________, KC_QUOT,
+    KC_LCTL, _________________QWERTY_L3_________________, _________________QWERTY_R1_________________, KC_ENT,
     KC_LALT, KC_4,    KC_3,    KC_2,    _______, KC_SPC,  KC_SPC,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid_wrapper(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  ___X___, ___X___, KC_EQL,  KC_MINS, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    ___X___, KC_BSLS,
-    KC_TAB,  ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_1,    KC_2,    KC_3,    KC_PPLS, KC_PMNS,
-    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_DOT,  KC_PAST, KC_PSLS
+    KC_GRV,  _________________LOWER_L1__________________, _________________LOWER_R1__________________, KC_BSPC,
+    KC_DEL,  _________________LOWER_L2__________________, _________________LOWER_R2__________________, KC_BSLS,
+    KC_TAB,  _________________LOWER_L3__________________, _________________LOWER_R3__________________, KC_PMNS,
+    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid_wrapper(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___X___, ___X___,
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  ___X___, ___X___,
+    KC_TILD, _________________RAISE_L1__________________, _________________RAISE_R1__________________, KC_BSPC,
+    KC_DEL,  _________________RAISE_L2__________________, _________________RAISE_R2__________________, ___X___,
+    _______, _________________RAISE_L3__________________, _________________RAISE_R3__________________, ___X___,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 [_ADJUST] = LAYOUT_planck_grid_wrapper(
-    _______, RESET,   DEBUG,   CG_TOGG, AU_TOG,  _______, _______, _______, _______, _______, _______, KC_DEL,
-    _______, _______, _______, _______, _______, _______, _______, QWERTY,  COLEMAK, GAME,    _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    RESET,   _________________ADJUST_L1_________________, _________________ADJUST_R1_________________, KC_DEL,
+    ___X___, _________________ADJUST_L2_________________, _________________ADJUST_R2_________________, ___X___,
+    ___X___, _________________ADJUST_L3_________________, _________________ADJUST_R3_________________, ___X___,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
-
-uint32_t layer_state_set_keymap(uint32_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
